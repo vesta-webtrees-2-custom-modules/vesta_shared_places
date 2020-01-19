fa2904793314ee837b3a635cd58ebf7882432504
Richard Cissee <ric@richard-cissee.de>
2020-01-19 16:46:50 +0100

    clean up autoloading of replaced classes

--- autoload.php
+++ autoload.php
@@ -9,13 +9,12 @@ $loader->addPsr4('Cissee\\WebtreesExt\\Services\\', __DIR__ . "/patchedWebtrees/
 $loader->addPsr4('Cissee\\WebtreesExt\\Functions\\', __DIR__ . "/patchedWebtrees/Functions");
 $loader->register();
 
-
+$classMap = array();
 $extend = !class_exists("Fisharebest\Webtrees\GedcomRecord", false);
         
 if ($extend) {
   //explicitly load webtrees replacements so that the original files aren't autoloaded
-  //(e.g. when routing via ModuleController)
-  require_once __DIR__ . '/replacedWebtrees/app/GedcomRecord.php';
+  $classMap["Fisharebest\Webtrees\GedcomRecord"] = __DIR__ . '/replacedWebtrees/GedcomRecord.php';
 
   //these adjustments allow use to re-use existing code (webtrees controllers using GedcomRecord::getInstance)
   //where we only have to adjust routes (e.g. for editing shared places, 
@@ -23,7 +22,7 @@ if ($extend) {
 
   $extend2 = !class_exists("Fisharebest\Webtrees\Html", false);
   if ($extend2) {
-    require_once __DIR__ . '/replacedWebtrees/Html.php';
+    $classMap["Fisharebest\Webtrees\Html"] = __DIR__ . '/replacedWebtrees/Html.php';
   }
 
   //
@@ -32,25 +31,25 @@ if ($extend) {
   //adjustments for MAP
   $extend3 = !class_exists("Fisharebest\Webtrees\Config", false);
   if ($extend3) {
-    require_once __DIR__ . '/replacedWebtrees/Config.php';
+    $classMap["Fisharebest\Webtrees\Config"] = __DIR__ . '/replacedWebtrees/Config.php';
   }
 
   //adjustments for MAP
   $extend4 = !class_exists("Fisharebest\Webtrees\Functions\FunctionsEdit", false);
   if ($extend4) {
-    require_once __DIR__ . '/replacedWebtrees/Functions/FunctionsEdit.php';
+    $classMap["Fisharebest\Webtrees\Functions\FunctionsEdit"] = __DIR__ . '/replacedWebtrees/Functions/FunctionsEdit.php';
   }
   
   //label for _GOV
   $extend5 = !class_exists("Fisharebest\Webtrees\GedcomTag", false);
   if ($extend5) {
-    require_once __DIR__ . '/replacedWebtrees/app/GedcomTag.php';
+    $classMap["Fisharebest\Webtrees\GedcomTag"] = __DIR__ . '/replacedWebtrees/GedcomTag.php';
   }
   
   //media links adjustments
   $extend5 = !class_exists("Fisharebest\Webtrees\Http\Controllers\Admin\MediaController", false);
   if ($extend5) {
-    require_once __DIR__ . '/replacedWebtrees/app/Http/Controllers/Admin/MediaController.php';
+    $classMap["Fisharebest\Webtrees\Http\Controllers\Admin\MediaController"] = __DIR__ . '/replacedWebtrees/Http/Controllers/Admin/MediaController.php';
   }
 } else {
   //must use original files because they are already loaded
@@ -58,3 +57,5 @@ if ($extend) {
   //thus cannot use GedcomRecord::getInstance etc here
 }
 
+$loader->addClassMap($classMap);        
+$loader->register(true); //prepend in order to override definitions from default class loader
