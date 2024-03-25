81c8dc7589134f5764d037d1f540e180fcf35548
Holger Eisold <github@eisold.family>
2024-03-25 07:04:34 +0100

    CodeStyle - remove whitespace
    Signed-off-by: Holger Eisold <github@eisold.family>

--- WhatsNew/WhatsNew0.php
+++ WhatsNew/WhatsNew0.php
@@ -5,7 +5,7 @@ namespace Cissee\Webtrees\Module\SharedPlaces\WhatsNew;
 use Cissee\WebtreesExt\WhatsNew\WhatsNewInterface;
 
 class WhatsNew0 implements WhatsNewInterface {
-  
+
   public function getMessage(): string {
     return "Vesta Shared Places: Option to use hierarchical shared places (enabled by default). See Readme and module configuration for details.";
   }
--- patchedWebtrees/PlaceViaSharedPlace.php
+++ patchedWebtrees/PlaceViaSharedPlace.php
@@ -23,25 +23,25 @@ use Vesta\Model\PlaceStructure;
 use Vesta\Model\Trace;
 
 class PlaceViaSharedPlace implements PlaceWithinHierarchy {
-  
+
   protected $actual;
-  
+
   /** @var bool */
   protected $asAdditionalParticipant;
-  
+
   /** @var PlaceUrls */
   protected $urls;
-  
+
   protected $module;
-          
+
   /** @var Collection */
   protected $sharedPlaces;
-  
+
   /** @var MapCoordinates|null */
   protected $latLon = null;
-  
+
   protected $latLonInitialized = false;
-  
+
   public function __construct(
           Place $actual,
           bool $asAdditionalParticipant,
@@ -54,22 +54,22 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
     $this->urls = $urls;
     $this->module = $module;
     $this->sharedPlaces = $sharedPlaces;
-  }  
-    
+  }
+
   //Speedup
   //more efficient than $this->actual->url() when calling for lots of places
   //this should be in webtrees, e.g. via caching result of 'findByComponent' or even 'Auth::accessLevel'
   public function url(): string {
     return $this->urls->url($this->actual);
   }
-  
+
   public function gedcomName(): string {
     return $this->actual->gedcomName();
   }
-  
+
   public function placeName(): string {
     $uniqueSharedPlaces = boolval($this->module->getPreference('UNIQUE_SP_IN_HIERARCHY', '0'));
-    
+
     if ($uniqueSharedPlaces && !$this->asAdditionalParticipant) {
       //by design only current names (exclude historical names!)
       $place_name = $this->sharedPlaces
@@ -79,19 +79,19 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
               ->reduce(function ($carry, $item): string {
                   return ($carry === "")?$item:$carry . " | " . $item;
               }, "");
-              
+
       return '<span dir="auto">' . e($place_name) . '</span>';
     }
-    
+
     return $this->actual->placeName();
   }
-  
+
   //cf DefaultPlaceWithinHierarchy, but map to SharedPlace/PlaceViaSharedPlace directly from query
   public function getChildPlacesCacheIds(
           Place $place): Collection {
-    
+
     $self = $this;
-    
+
     if ($place->gedcomName() !== '') {
         $parent_text = Gedcom::PLACE_SEPARATOR . $place->gedcomName();
     } else {
@@ -105,12 +105,12 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
         ->where('p_parent_id', '=', $place->id())
         ->join('placelinks', static function (JoinClause $join): void {
             $join
-                ->on('pl_file', '=', 'p_file')                    
+                ->on('pl_file', '=', 'p_file')
                 ->on('pl_p_id', '=', 'p_id');
         })
         ->join('other', static function (JoinClause $join): void {
             $join
-                ->on('o_file', '=', 'pl_file')                    
+                ->on('o_file', '=', 'pl_file')
                 ->on('o_id', '=', 'pl_gid');
         })
         ->where('o_type', '=', '_LOC')
@@ -119,7 +119,7 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
             $place = new Place($row->p_place . $parent_text, $tree);
             $id = $row->p_id;
             Registry::cache()->array()->remember('place-' . $place->gedcomName(), function () use ($id): int {return $id;});
-            
+
             $sharedPlace = Registry::locationFactory()->mapper($tree)($row);
             return ["actual" => $place, "record" => $sharedPlace];
         })
@@ -129,17 +129,17 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
             $names = new Collection($item["record"]->namesAsPlacesAt(GedcomDateInterval::createEmpty()));
             return $names->has($item["actual"]->id());
         });
-    
+
     if ($place2sharedPlaceMap->isEmpty()) {
       return new Collection();
     }
-    
+
     $uniqueSharedPlaces = boolval($this->module->getPreference('UNIQUE_SP_IN_HIERARCHY', '0'));
-    
+
     //if $asAdditionalParticipant, we just need the additional data (map coordinates)
     //and do not evaluate $uniqueSharedPlaces
     if ($uniqueSharedPlaces && !$this->asAdditionalParticipant) {
-      $place2sharedPlaceMap = $place2sharedPlaceMap  
+      $place2sharedPlaceMap = $place2sharedPlaceMap
         ->mapToGroups(static function ($item): array {
           /** @var SharedPlace $sharedPlace */
           $sharedPlace = $item["record"];
@@ -149,7 +149,7 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
           $first = $groupedItems->first();
           /** @var SharedPlace $sharedPlace */
           $sharedPlace = $first["record"];
-          
+
           //which place to use? follow order from shared place
           $place = null;
           foreach ($sharedPlace->namesAsPlacesAt(GedcomDateInterval::createEmpty()) as $placeViaSharedPlace) {
@@ -160,15 +160,15 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
               }
             }
           }
-          
+
           if ($place === null) {
             throw new Exception("unexpected null place!");
           }
           return ["actual" => $place, "record" => $sharedPlace];
         });
     }
-        
-    $childPlaces = $place2sharedPlaceMap  
+
+    $childPlaces = $place2sharedPlaceMap
         ->mapToGroups(static function ($item): array {
           $place = $item["actual"];
           return [$place->id() => $item];
@@ -184,10 +184,10 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
                   });
 
           return new PlaceViaSharedPlace(
-                  $first["actual"], 
-                  $self->asAdditionalParticipant, 
-                  $self->urls, 
-                  $sharedPlaces, 
+                  $first["actual"],
+                  $self->asAdditionalParticipant,
+                  $self->urls,
+                  $sharedPlaces,
                   $self->module);
         })
         ->sort(static function (PlaceViaSharedPlace $x, PlaceViaSharedPlace $y): int {
@@ -196,54 +196,54 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
         ->mapWithKeys(static function (PlaceViaSharedPlace $place): array {
           return [$place->id() => $place];
         });
-        
-    return $childPlaces;    
+
+    return $childPlaces;
   }
-  
+
   public function getChildPlaces(): array {
     $ret = $this
             ->getChildPlacesCacheIds($this->actual)
             ->toArray();
-    
+
     return $ret;
   }
-  
+
   public function id(): int {
     return $this->actual->id();
   }
-  
+
   public function tree(): Tree {
     return $this->actual->tree();
   }
-  
+
   public function fullName(bool $link = false): string {
     return $this->actual->fullName($link);
   }
-  
+
   public function searchIndividualsInPlace(): Collection {
     return SharedPlace::linkedIndividualsRecords($this->sharedPlaces);
   }
-  
+
   public function countIndividualsInPlace(): int {
     return SharedPlace::linkedIndividualsCount($this->sharedPlaces);
   }
-  
+
   public function searchFamiliesInPlace(): Collection {
     return SharedPlace::linkedFamiliesRecords($this->sharedPlaces);
   }
-  
+
   public function countFamiliesInPlace(): int {
     return SharedPlace::linkedFamiliesCount($this->sharedPlaces);
   }
-  
+
   protected function initLatLon(): ?MapCoordinates {
     $useIndirectLinks = boolval($this->module->getPreference('INDIRECT_LINKS', '1'));
-    
+
     if (!$useIndirectLinks) {
       //check shared places directly, they won't be checked via plac2map
       foreach ($this->sharedPlaces as $sharedPlace) {
         /* @var $sharedPlace SharedPlace */
-        
+
         $locReference = new LocReference($sharedPlace->xref(), $sharedPlace->tree(), new Trace(''));
         $mapCoordinates = FunctionsPlaceUtils::loc2map($this->module, $locReference);
         if ($mapCoordinates !== null) {
@@ -251,26 +251,26 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
         }
       }
     }
-    
+
     $ps = $this->placeStructure();
     if ($ps === null) {
       return null;
     }
     return FunctionsPlaceUtils::plac2map($this->module, $ps, false);
   }
-  
+
   public function getLatLon(): ?MapCoordinates {
     if (!$this->latLonInitialized) {
       $this->latLon = $this->initLatLon();
       $this->latLonInitialized = true;
     }
-    
+
     return $this->latLon;
   }
-  
+
   public function latitude(): ?float {
     //we don't go up the hierarchy here - there may be more than one parent!
-    
+
     $lati = null;
     if ($this->getLatLon() !== null) {
       $lati = $this->getLatLon()->getLati();
@@ -278,14 +278,14 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
     if ($lati === null) {
       return null;
     }
-    
+
     $gedcom_service = new GedcomService();
     return $gedcom_service->readLatitude($lati);
   }
-  
+
   public function longitude(): ?float {
     //we don't go up the hierarchy here - there may be more than one parent!
-    
+
     $long = null;
     if ($this->getLatLon() !== null) {
       $long = $this->getLatLon()->getLong();
@@ -293,15 +293,15 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
     if ($long === null) {
       return null;
     }
-    
+
     $gedcom_service = new GedcomService();
     return $gedcom_service->readLongitude($long);
   }
-  
+
   public function icon(): string {
     return '';
   }
-  
+
   public function boundingRectangleWithChildren(array $children): array
   {
       /*
@@ -313,15 +313,15 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
 
       $latitudes = [];
       $longitudes = [];
-      
+
       if ($this->latitude() !== null) {
         $latitudes[] = $this->latitude();
       }
       if ($this->longitude() !== null) {
         $longitudes[] = $this->longitude();
       }
-      
-      foreach ($children as $child) { 
+
+      foreach ($children as $child) {
         if ($child->latitude() !== null) {
           $latitudes[] = $child->latitude();
         }
@@ -333,12 +333,12 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
       if ((count($latitudes) === 0) || (count($longitudes) === 0)) {
         return [[-180.0, -90.0], [180.0, 90.0]];
       }
-      
+
       $latiMin = (new Collection($latitudes))->min();
       $longMin = (new Collection($longitudes))->min();
       $latiMax = (new Collection($latitudes))->max();
       $longMax = (new Collection($longitudes))->max();
-      
+
       //never zoom in too far (in particular if there is only one place, but also if the places are close together)
       $latiSpread = $latiMax - $latiMin;
       if ($latiSpread < 1) {
@@ -351,7 +351,7 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
         $longMin -= (1 - $longSpread)/2;
         $longMax += (1 - $longSpread)/2;
       }
-      
+
       return [[$latiMin, $longMin], [$latiMax, $longMax]];
   }
 
@@ -361,19 +361,19 @@ class PlaceViaSharedPlace implements PlaceWithinHierarchy {
 
   public function additionalLinksHtmlBeforeName(): string {
     $html = '';
-    if ($this->module !== null) {      
+    if ($this->module !== null) {
       foreach ($this->sharedPlaces as $sharedPlace) {
         $html .= $this->module->getLinkForSharedPlace($sharedPlace);
       }
     }
-    
+
     return $html;
   }
-  
+
   public function links(): Collection {
     return $this->urls->links($this->actual);
   }
-  
+
   public function parent(): PlaceWithinHierarchy {
     return $this->module->findPlace($this->actual->parent()->id(), $this->actual->tree(), $this->urls);
   }
